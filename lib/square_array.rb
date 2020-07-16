0dc70bda7a3c938ada2e39477225fd8515bda9de
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-16 14:51:47 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  counter=0
+  new_array=[]
+  while counter<array.length do
+    num = array[counter]
+    product=num*num
+    new_array.push(product)
+    counter+=1
+  end
+  new_array
 end
